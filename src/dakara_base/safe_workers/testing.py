0e97b3f0a5e0d16b0a46f61c47a35e7c9b0bf105
Neraste <neraste.herr10@gmail.com>
2026-09-07 00:17:44 +0200

    Improve documentation

--- src/dakara_base/safe_workers/testing.py
+++ src/dakara_base/safe_workers/testing.py
@@ -1,3 +1,25 @@
+"""Module for testing functions related to safe workers.
+
+The helper `assert_worker_no_errors` checks if a worker does not have errors:
+
+>>> from dakara_base.safe_workers import WorkerSafeThread
+>>> from threading import Event
+>>> from queue import Queue
+>>> worker = WorkerSafeThread(Event(), Queue())
+>>> assert_worker_no_errors(worker)
+>>> def error_function():
+...     raise Exception("error message")
+>>> worker.set_thread(target=error_function)
+>>> worker.thread.start()
+>>> worker.thread.join()
+>>> assert_worker_no_errors(worker)
+Traceback (most recent call last):
+    ...
+AssertionError: Worker WorkerSafeThread(...) is in failed state:
+0: Exception: error message
+  File ...
+"""
+
 from traceback import format_tb
 
 from dakara_base.safe_workers import BaseWorker
@@ -5,7 +27,7 @@ from dakara_base.safe_workers import BaseWorker
 
 def assert_worker_no_errors(worker: BaseWorker) -> None:
     """Assert that a worker is not in failed state, otherwise raise an
-    exception containing all its errors.
+    exception containing all the error messages of the captured exceptions..
 
     Args:
         worker: Instance of the worker.
